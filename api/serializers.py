daec11d59a658ef12de691c47c930848b3c4e4f0
Imanol <imanol.98@hotmail.com>
2022-06-03 05:04:52 +0200

    flake8 config and fixes

--- api/serializers.py
+++ api/serializers.py
@@ -1,19 +1,22 @@
 from rest_framework import serializers
 
+
 class productSerializer(serializers.Serializer):
     name = serializers.CharField()
     id = serializers.IntegerField()
     depth = serializers.IntegerField()
 
+
 class bookSerializer(serializers.Serializer):
     title = serializers.CharField()
     description = serializers.CharField()
-    
+
 
 class userSerializer(serializers.Serializer):
     id = serializers.IntegerField()
     email = serializers.EmailField()
     username = serializers.CharField()
+
 
 class basketSerializer(serializers.Serializer):
     id = serializers.IntegerField()
